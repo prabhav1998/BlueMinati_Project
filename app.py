ebabc5182a92a98ba5e01a87037e70ac1c6082ef
BlueMinati <64685442+prabhav1998@users.noreply.github.com>
2022-12-06 23:48:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask,render_template
 
 app = Flask('__name__')
 
+#@app.route('/')
+#def home():
+#    return "Hello World ! , HEYYY this is a Flask Application prabhav 11:43"
 @app.route('/')
 def home():
-    return "Hello World ! , HEYYY this is a Flask Application prabhav 11:43"
+    return render_template('main.html')
 
 
 
