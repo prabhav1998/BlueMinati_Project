9c86ecc1ed44f5e75771ef4db35e0a6279364590
BlueMinati <64685442+prabhav1998@users.noreply.github.com>
2022-12-06 23:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ app = Flask('__name__')
 
 @app.route('/')
 def home():
-    return "Hello World ! , this is a Flask Application 8.... .prabhav"
+    return "Hello World ! , this is a Flask Application 8. .prabhav"
 
 
-@app.route('/stopServer', methods=['GET'])
-def stopServer():
-    os.kill(os.getpid(), signal.SIGINT)
-    return 0 #jsonify({ "success": True, "message": "Server is shutting down..." })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
