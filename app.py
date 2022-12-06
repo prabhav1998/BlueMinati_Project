dea536b89466fd4fc422b22d549e89c810d28a5f
BlueMinati <64685442+prabhav1998@users.noreply.github.com>
2022-12-06 21:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,10 @@ from flask import Flask,render_template
 
 app = Flask('__name__')
 
-
 @app.route('/')
 def home():
-    return render_template('main.html')
-
-@app.route('/endpoint')
-def endpoint():
     return "Hello World ! , this is a Flask Application"
 
-@app.route('/linker')
-def linker():
-    return render_template('link.html')
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug =True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
 
